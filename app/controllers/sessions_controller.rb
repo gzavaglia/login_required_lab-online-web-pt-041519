dcf59403c4305e9f83c2fccb9fb83a6be48692ef
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-10-03 18:30:09 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < ApplicationController
   end
   
   def create
-    if params[:name]/
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to :root
+    else
+      redirect_to '/'
+    end
   end
   
   def destroy
