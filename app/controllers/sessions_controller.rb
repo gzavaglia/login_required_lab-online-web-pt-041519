6901986aec7c831eba470414c549a9f359148aba
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-10-03 18:31:07 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     if params[:name].present?
       session[:name] = params[:name]
-      redirect_to :root
+      redirect_to '/'
     else
       redirect_to :login
     end
